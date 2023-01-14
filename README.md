038f2ec3a8df2f97422b3ba03115c4ef081b4001
iamgame3 <kosmosworld9@hotmail.com>
2023-01-14 15:35:09 -0500

    Add tools needed for development, basic project files, and add project description to README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# restaurant-page
+# Restaurant Page Project (WIP)
+
+This project is meant to test my ablity to effectively use Webpack, JS modules, and show advanced control over the DOM.
+
+Pretty intuitive, just click the tabs at the top of the page to switch between various menu sections.
+
+P.S. This project is a part of The Odin Project's course.
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
