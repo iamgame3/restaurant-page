0c30746573a98db771a966fe25b3b52ebea57aee
iamgame3 <kosmosworld9@hotmail.com>
2023-01-16 21:09:29 -0500

    Add menu, section selection, and section selection buttons

--- src/page-setup.js
+++ src/page-setup.js
@@ -8,6 +8,30 @@ const pageSetup = () => {
   header.textContent = "Meat Eater's Diner";
   header.classList.add("header");
   content.appendChild(header);
+
+  // Create the menu and add it to the page
+  const menu = document.createElement("div");
+  menu.classList.add("menu");
+  content.appendChild(menu);
+
+  // Create menu section selection and add it to the menu
+  const sectionSelection = document.createElement("div");
+  sectionSelection.classList.add("section-selection");
+  menu.appendChild(sectionSelection);
+
+  // Create menu selection buttons and add them to the section selection
+  const entreesButton = document.createElement("button");
+  const mainMealsButton = document.createElement("button");
+  const desertsButton = document.createElement("button");
+  entreesButton.textContent = "Entrées";
+  mainMealsButton.textContent = "Main Meals";
+  desertsButton.textContent = "Deserts";
+  const menuButtons = [entreesButton, mainMealsButton, desertsButton];
+  menuButtons.forEach((button) => {
+    button.classList.add("section-button");
+    button.classList.add("secton-button:active");
+    sectionSelection.appendChild(button);
+  });
 };
 
 export default pageSetup();
